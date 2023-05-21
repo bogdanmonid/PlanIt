af808d2fbcbbef4065c81765bde1cd9f59af95ff
bogdanmonid <119861628+bogdanmonid@users.noreply.github.com>
2023-05-21 20:19:29 +0300

    Initial Commit

---
+++ PlanIt/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PlanIt
+//
+//  Created by Bogdan Monid on 21.05.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
