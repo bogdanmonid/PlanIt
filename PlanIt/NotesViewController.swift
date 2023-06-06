13c80ed55c2af3c91f7e149303901ce0753aa5bc
bogdanmonid <119861628+bogdanmonid@users.noreply.github.com>
2023-06-06 18:03:04 +0300

    add shadow for textView and textField

--- PlanIt/NotesViewController.swift
+++ PlanIt/NotesViewController.swift
@@ -17,13 +17,13 @@ class NotesViewController: UIViewController{
     @IBOutlet weak var textViewLabel: UITextView!{
         didSet{
             textViewLabel.layer.cornerRadius = 13
-           
+            
         }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-       
+        
         saveButton.isEnabled = true
         textFieldLabel.addTarget(self, action: #selector(saveEnabling), for: .editingChanged)
         
@@ -43,7 +43,9 @@ class NotesViewController: UIViewController{
         super.viewDidLayoutSubviews()
         
         textFieldLabel.layer.cornerRadius = 17
-        textFieldLabel.addShadow(to: [.top, .left, .right])
+        textFieldLabel.addShadow()
+        textViewLabel.addShadowForTextView()
+        
     }
     
     @IBAction func saveAction(_ sender: Any) {
@@ -70,43 +72,36 @@ extension NotesViewController: UITextFieldDelegate{
     }
 }
 
-extension UITextField{
+extension UIView {
+    func addShadow() {
+        
+        let innerShadow = CALayer()
+        innerShadow.frame = bounds
+        let radius = self.frame.size.height / 2
+        let path = UIBezierPath(roundedRect: innerShadow.bounds.insetBy(dx: -1, dy: -1), cornerRadius: radius)
+        let cutout = UIBezierPath(roundedRect: innerShadow.bounds, cornerRadius: radius).reversing()
+        
+        path.append(cutout)
+        innerShadow.shadowPath = path.cgPath
+        innerShadow.masksToBounds = true
+        innerShadow.shadowColor = UIColor.black.cgColor
+        innerShadow.shadowOffset = CGSize(width: 1, height: 3)
+        innerShadow.shadowOpacity = 1
+        innerShadow.shadowRadius = 2
+        innerShadow.cornerRadius = self.frame.size.height / 2
+        
+        layer.addSublayer(innerShadow)
+    }
     
-    func addShadow(to edges: [UIRectEdge], radius: CGFloat = 3, opacity: Float = 0.6, color: CGColor = UIColor.black.cgColor){
-        
-        let fromColor = color
-        let toColor = UIColor.clear.cgColor
-        let viewFrame = self.frame
-        
-        for edge in edges {
-            let  gradientLayer = CAGradientLayer()
-            gradientLayer.colors = [fromColor, toColor]
-            gradientLayer.opacity = opacity
-            gradientLayer.cornerRadius = 17
-            gradientLayer.masksToBounds = true
-
-            switch edge{
-            case .top:
-                gradientLayer.startPoint = CGPoint(x: 0.5, y: 0)
-                gradientLayer.endPoint = CGPoint(x: 0.5, y: 1)
-                gradientLayer.frame = CGRect(x: 0, y: 0, width: viewFrame.width, height: radius)
-            case .bottom:
-                gradientLayer.startPoint = CGPoint(x: 0.5, y: 1)
-                gradientLayer.endPoint = CGPoint(x: 0.5, y: 0)
-                gradientLayer.frame = CGRect(x: 0, y: viewFrame.height - radius, width: viewFrame.width, height: radius)
-            case .left:
-                gradientLayer.startPoint = CGPoint(x: 0, y: 0.5)
-                gradientLayer.endPoint = CGPoint(x: 1, y: 0.5)
-                gradientLayer.frame = CGRect(x: 0, y: 0, width: radius, height: viewFrame.height)
-            case.right:
-                gradientLayer.startPoint = CGPoint(x: 1, y: 0.5)
-                gradientLayer.endPoint = CGPoint(x: 0, y: 0.5)
-                gradientLayer.frame = CGRect(x: viewFrame.width - radius, y: 0, width: radius, height: viewFrame.height)
-            default:
-                break
-            }
-            self.layer.addSublayer(gradientLayer)
-        }
+    func addShadowForTextView(shadowColor: CGColor = UIColor.black.cgColor,
+                              shadowOffset: CGSize = CGSize(width: 1.0, height: 2.0),
+                              shadowOpacity: Float = 0.4,
+                              shadowRadius: CGFloat = 3.0){
+        layer.shadowColor = shadowColor
+        layer.shadowOffset = shadowOffset
+        layer.shadowOpacity = shadowOpacity
+        layer.shadowRadius = shadowRadius
+        layer.masksToBounds = false
     }
 }
 
